fb49ae6903b75cdbe16d66851ee78a6a82bc3d2c
JS-SG <144525322+JS-SG@users.noreply.github.com>
2026-03-17 12:35:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,15 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const db = new sqlite3.Database("./workflow.db", (err) => {
- if (err) console.error(err.message);
- console.log("Connected to SQLite database.");
+const dbPath =
+  process.env.DB_PATH || path.join(__dirname, "../workflow.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Connected to SQLite database:", dbPath);
+  }
 });
 
 module.exports = db;
