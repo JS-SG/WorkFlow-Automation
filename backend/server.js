0ffb3f86509fb7b47c8731d920e73efe408b2040
JS-SG <144525322+JS-SG@users.noreply.github.com>
2026-03-17 12:42:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ require("./database/schema");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ].filter(Boolean),
     methods: ["GET","POST","PUT","DELETE","PATCH"],
     credentials: true
 }));
